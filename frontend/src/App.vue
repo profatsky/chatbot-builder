1755668620c4a7800cc38b51a4d3eda1fb96ba3d
Mikhail Profatsky <mixail.rostov04@gmail.com>
2024-04-03 21:41:34 +0300

    init frontend structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/router/router.js
@@ -0,0 +1,16 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import Main from '@/pages/Main.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Main
+  },
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHistory()
+})
+
+export default router;
