998b77a345f66df04f6d4c32785e89ebf1ecc142
Mikhail Profatsky <mixail.rostov04@gmail.com>
2025-01-28 12:54:50 +0300

    feat: added pytest fixtures

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -14,6 +14,7 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
+TEST_DB_NAME = os.environ.get('TEST_DB_NAME')
 
 # Email sender
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
---
+++ backend/tests/conftest.py
@@ -0,0 +1,56 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+from src.core import settings
+from src.core.db import Base, get_async_session, get_postgres_dsn
+from src.main import app
+
+SQLALCHEMY_DATABASE_URL = get_postgres_dsn(
+    user=settings.DB_USER,
+    password=settings.DB_PASS,
+    host=settings.DB_HOST,
+    port=settings.DB_PORT,
+    database=settings.TEST_DB_NAME,
+)
+
+async_engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+async_session_maker = async_sessionmaker(
+    expire_on_commit=False,
+    bind=async_engine,
+    class_=AsyncSession
+)
+
+
+@pytest_asyncio.fixture(scope='session')
+async def session():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with async_session_maker() as session:
+        yield session
+
+
+@pytest_asyncio.fixture(scope='session')
+async def client(session):
+    async def override_get_session():
+        try:
+            yield session
+        finally:
+            await session.close()
+
+    app.dependency_overrides[get_async_session] = override_get_session
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url='http://127.0.0.1:8000/api',
+    ) as cli:
+        yield cli
+
+
+def pytest_collection_modifyitems(items):
+    pytest_asyncio_tests = (item for item in items if pytest_asyncio.is_async_test(item))
+    session_scope_marker = pytest.mark.asyncio(loop_scope='session')
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
