9f73267f50706bf097ee765eb593ea33cab6060f
Mikhail Profatsky <mixail.rostov04@gmail.com>
2025-02-06 00:05:53 +0300

    feat: added factory for projects create schema

---
+++ backend/tests/factories/projects.py
@@ -0,0 +1,16 @@
+import factory
+from faker import Faker
+
+from src.enums import KeyboardType
+from src.projects.schemas import ProjectCreateSchema
+
+fake = Faker()
+
+
+class ProjectCreateSchemaFactory(factory.Factory):
+    class Meta:
+        model = ProjectCreateSchema
+
+    name = factory.LazyFunction(lambda: fake.sentence(nb_words=3))
+    start_message = factory.LazyFunction(lambda: fake.sentence(nb_words=6))
+    start_keyboard_type = factory.Iterator([kb_type.value for kb_type in KeyboardType])
