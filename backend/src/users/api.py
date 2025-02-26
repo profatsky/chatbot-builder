61c7f362bb698df5c9075e4f069dddaabb18f75d
Mikhail Profatsky <mixail.rostov04@gmail.com>
2025-02-26 23:50:18 +0300

    refactor: added http exception class for 401 status

--- backend/src/users/api.py
+++ backend/src/users/api.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, Depends
 
 from src.auth.dependencies.auth_dependencies import UserIDFromAccessTokenDI, access_token_required
 from src.users.dependencies.services_dependencies import UserServiceDI
+from src.users.exceptions.http_exceptions import UnauthorizedHTTPException
 from src.users.exceptions.services_exceptions import UserNotFoundError
 from src.users.schemas import UserWithStatsReadSchema
 
@@ -23,7 +24,4 @@ async def get_user(
     try:
         return await user_service.get_user_with_stats(user_id)
     except UserNotFoundError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Unauthorized user'
-        )
+        raise UnauthorizedHTTPException
--- backend/src/users/exceptions/http_exceptions.py
+++ backend/src/users/exceptions/http_exceptions.py
@@ -15,3 +15,11 @@ class UserAlreadyExistsHTTPException(HTTPException):
             status_code=status.HTTP_409_CONFLICT,
             detail='User with this email address is already registered',
         )
+
+
+class UnauthorizedHTTPException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Unauthorized user',
+        )
